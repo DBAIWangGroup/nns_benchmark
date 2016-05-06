91a05cabbdd2c24557004c85c9f1efea1a72abcf
yifangs <yifang.bill.sun@gmail.com>
2016-05-07 00:08:44 +1000

    Added files via upload

---
+++ algorithms/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/bunit.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/bunit.cc.o"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/test_distfunc.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/test_distfunc.cc.o"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/test_editdist.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/test_editdist.cc.o"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/test_eval.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/test_eval.cc.o"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/test_fp.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/test_fp.cc.o"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/test_lpnorm.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/test_lpnorm.cc.o"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/test_object.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/test_object.cc.o"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/test_proj.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/test_proj.cc.o"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/test_sqfd.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/test_sqfd.cc.o"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/test/test_timer.cc" "/home/yzhang4/HD/release/NonMetricSpaceLib/test/CMakeFiles/bunit.dir/test_timer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/src/CMakeFiles/NonMetricSpaceLib.dir/DependInfo.cmake"
+  "/home/yzhang4/HD/release/NonMetricSpaceLib/lshkit/CMakeFiles/lshkit.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "lshkit/include"
+  "/usr/include/eigen3"
+  "/home/yzhang4/HD/home/liwen/Downloads/software/boost1.58/include"
+  "src"
+  "include"
+  "include/space"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
