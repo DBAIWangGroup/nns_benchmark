655fa175074c4a67a528a4850d533057e405e5fb
yingzhang <yingz.au@gmail.com>
2016-05-19 18:14:17 +1000

    update

--- algorithms/KGraph/script/kgraph_index.sh
+++ algorithms/KGraph/script/kgraph_index.sh
@@ -4,6 +4,7 @@ bin_dir="../src"
 
 mkdir ${index_dir} 
 
+
 # Please sure the data ( data points, query points and ground truth files are located at ../../../data directory )
 
 for data in audio # sift 
@@ -11,12 +12,11 @@ do
 
 # L is for lengh of the NN-list (i.e.,#neighbors materialized) of the  K-NN graph
 # the K value of the input is the desirable k value in the k-NN search 
-for L in 20 40
-do
+
+L=40
 
 ${bin_dir}/kgraph_index --data ${data_dir}/${data}_base.lshkit --output ${index_dir}/${data}.kgraph_${L} -S 20 -L ${L} -K 20
 
-done
 done
 
 
